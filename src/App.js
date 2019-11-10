3767d5276976700badec9fd9af2aede37da6a1e7
Moises Perez <moypg99@gmail.com>
2019-11-10 13:33:41 -0600

    Primer commit
    Primer commit desde github desktop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => (
+
+  <Fragment>
+    <div>
+      <h1>Hola Mundo</h1>
+      <p>Saludando desde mi componente</p>
+
     </div>
-  );
-}
+    <div>
+      <h2>Este es un h2</h2>
+      <img src="https://static-s.aa-cdn.net/img/ios/749057895/65b92c35258e6465d327f0e1dd89e8d4"/>
+    </div>
+
+  </Fragment>
+
+)
 
 export default App;
+
+//Reglas
+//1. Toda etiqueta debe cerrarse
+//2. Los componentes deben devolver solo un componente padre
+//3. Apoyarse de los fragmentos
+//4. Fragment || <> hijos </>
+//5
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
